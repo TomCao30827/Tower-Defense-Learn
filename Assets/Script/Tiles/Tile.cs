70558801350de528ffde53a9759beed200d40de5
TomCao30827 <cnhnhan03@gmail.com>
2024-04-19 13:55:22 +0700

    Blockin note

--- Assets/Script/Tiles/Tile.cs
+++ Assets/Script/Tiles/Tile.cs
@@ -10,6 +10,27 @@ public class Tile : MonoBehaviour
     [SerializeField] private bool _isPlacable = true;
     public bool IsPlacable { get { return _isPlacable; } }
 
+    private GridManager gridManager;
+    private Vector2Int coordinates = new Vector2Int();
+
+    void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+    }
+
+    void Start()
+    {
+        if (gridManager != null)
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+
+            if (!_isPlacable)
+            {
+                gridManager.BlockNode(coordinates);
+            }
+        }
+    }
+
 
     private void OnMouseDown()
     {
